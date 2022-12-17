30d05474101f3e87f2886c414789c796f536be26
jdkfx <amateurranch@gmail.com>
2022-12-18 01:46:23 +0900

    :art: 背景画像の追加

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  position: relative;
+  overflow: hidden;
+  z-index: 0;
+}
+
+body::before {
+  content: '';
+  position: absolute;
+  background-image: url('/src/assets/images/stageL/Vss_Yunohana.png');
+  background-size: cover;
+  filter: blur(5px);
+  top: -3px;
+  bottom: -3px;
+  left: -3px;
+  right: -3px;
+  z-index: -1;
 }
 </style>
